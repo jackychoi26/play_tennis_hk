8bdb0fb5efb2feced9d427a15eab0bc77d9f1edf
charleskan <71898304+charleskan@users.noreply.github.com>
2023-07-14 01:07:49 +0800

    ADD create match ui ver 1 done

--- lib/constants/app_ui_style.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-
-const TextStyle deepPurpleTitleBold = TextStyle(
-  fontSize: 12,
-  fontWeight: FontWeight.bold,
-  color: Colors.deepPurple,
-);
--- lib/features/matchmaking/ui/create_tennis_match_screen.dart
+++ lib/features/matchmaking/ui/create_tennis_match_screen.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:intl/intl.dart';
 import 'package:play_tennis_hk/components/custom_text.dart';
 import 'package:play_tennis_hk/components/custom_text_form_field.dart';
+import 'package:play_tennis_hk/components/show_date_time_picker.dart';
 import 'package:play_tennis_hk/domain/match_type.dart';
-import 'package:play_tennis_hk/features/matchmaking/ui/remark.dart';
 import 'package:play_tennis_hk/features/profile/ui/districts_list.dart';
 import 'package:play_tennis_hk/features/profile/ui/usta_level_dropdown_selection.dart';
 
@@ -18,11 +19,24 @@ class CreateTennisMatchScreen extends ConsumerStatefulWidget {
 
 class CreateTennisMatchScreenState
     extends ConsumerState<CreateTennisMatchScreen> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  final _formKey = GlobalKey<FormState>();
+
   var locationController = TextEditingController();
 
   var remarksController = TextEditingController();
 
+  DateTime startDateTime = DateTime.now();
+
+  DateTime endDateTime = DateTime.now();
+
   MatchType dropdownValue = MatchType.singles;
+
+  DateFormat dateFormat = DateFormat('yyyy-MM-dd HH:mm');
 
   @override
   Widget build(BuildContext context) {
@@ -33,47 +47,105 @@ class CreateTennisMatchScreenState
           textType: CustomTextType.subtitle,
         ),
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          CustomTextFormField(
-            controller: locationController,
-            textInputType: TextInputType.visiblePassword,
-            isPassword: true,
-            labelText: AppLocalizations.of(context)?.location,
-          ),
-          const USTALevelDropdownSelection(),
-          Container(
-            margin: const EdgeInsets.all(16),
-            child: const DistrictsList(),
-          ),
-          DropdownButton<MatchType>(
-              padding: const EdgeInsets.symmetric(horizontal: 16),
-              value: dropdownValue,
-              icon: const Icon(Icons.arrow_downward),
-              elevation: 16,
-              style: const TextStyle(color: Colors.deepPurple),
-              onChanged: (MatchType? value) {
-                setState(() {
-                  dropdownValue = value!;
-                });
-              },
-              items: MatchType.values
-                  .map<DropdownMenuItem<MatchType>>((MatchType value) {
-                return DropdownMenuItem<MatchType>(
-                  value: value,
-                  child: Text(value.toLocalizedName(context) ?? ""),
-                );
-              }).toList()),
-          SizedBox(
-            height: 200,
-            width: 200,
-            child: Remarks(
-              remarksController: remarksController,
+      body: Form(
+        key: _formKey,
+        child: ListView(
+          children: [
+            Container(
+              alignment: Alignment.centerLeft,
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Row(
+                children: [
+                  CustomText(AppLocalizations.of(context)?.startDateTime ?? ""),
+                  TextButton(
+                    onPressed: () async {
+                      final DateTime? selectedDate = await showDateTimePicker(
+                        context: context,
+                        initialDate: DateTime.now(),
+                        firstDate: DateTime.now()
+                            .subtract(const Duration(days: 365 * 100)),
+                        lastDate:
+                            DateTime.now().add(const Duration(days: 365 * 200)),
+                      );
+                      if (selectedDate == null) return;
+
+                      setState(() {
+                        startDateTime = selectedDate;
+                      });
+                    },
+                    child: CustomText(
+                      dateFormat.format(startDateTime),
+                    ),
+                  ),
+                ],
+              ),
             ),
-          ),
-        ],
+            Container(
+              alignment: Alignment.centerLeft,
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Row(
+                children: [
+                  CustomText(AppLocalizations.of(context)?.endDateTime ?? ""),
+                  TextButton(
+                    onPressed: () async {
+                      final DateTime? selectedDate = await showDateTimePicker(
+                        context: context,
+                        initialDate: DateTime.now(),
+                        firstDate: DateTime.now()
+                            .subtract(const Duration(days: 365 * 100)),
+                        lastDate:
+                            DateTime.now().add(const Duration(days: 365 * 200)),
+                      );
+                      if (selectedDate == null) return;
+
+                      setState(() {
+                        endDateTime = selectedDate;
+                      });
+                    },
+                    child: CustomText(dateFormat.format(endDateTime)),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              alignment: Alignment.centerLeft,
+              child: DropdownButton<MatchType>(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  value: dropdownValue,
+                  icon: const Icon(Icons.arrow_downward),
+                  elevation: 16,
+                  style: const TextStyle(color: Colors.deepPurple),
+                  onChanged: (MatchType? value) {
+                    setState(() {
+                      dropdownValue = value!;
+                    });
+                  },
+                  items: MatchType.values
+                      .map<DropdownMenuItem<MatchType>>((MatchType value) {
+                    return DropdownMenuItem<MatchType>(
+                      value: value,
+                      child: Text(value.toLocalizedName(context) ?? ""),
+                    );
+                  }).toList()),
+            ),
+            const USTALevelDropdownSelection(),
+            Container(
+              margin: const EdgeInsets.all(20),
+              child: const DistrictsList(),
+            ),
+            CustomTextFormField(
+              controller: locationController,
+              textInputType: TextInputType.visiblePassword,
+              labelText: AppLocalizations.of(context)?.location,
+            ),
+            CustomTextFormField(
+              controller: remarksController,
+              textInputType: TextInputType.visiblePassword,
+              labelText: AppLocalizations.of(context)?.remarks,
+              maxLines: 3,
+            ),
+          ],
+        ),
       ),
     );
   }
