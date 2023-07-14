ecadc5696c0b858696ae50bd1dc1b593cac10978
charles.kan <>
2023-07-14 18:22:07 +0800

    feat added create match form validation && some ui updated.

--- lib/features/matchmaking/ui/create_tennis_match_screen.dart
+++ lib/features/matchmaking/ui/create_tennis_match_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:intl/intl.dart';
+import 'package:play_tennis_hk/components/custom_snack_bar.dart';
 import 'package:play_tennis_hk/components/custom_text.dart';
 import 'package:play_tennis_hk/components/custom_text_form_field.dart';
 import 'package:play_tennis_hk/components/show_date_time_picker.dart';
@@ -29,8 +30,6 @@ class CreateTennisMatchScreenState
 
   var locationController = TextEditingController();
   var remarksController = TextEditingController();
-  var startDateController = TextEditingController();
-  var endDateController = TextEditingController();
 
   DateTime? startDateTime;
 
@@ -42,13 +41,51 @@ class CreateTennisMatchScreenState
 
   List<District> selectedDistricts = [];
 
+  void onSaveSelectDistrict(List<District> newSelectedDistricts) {
+    selectedDistricts = newSelectedDistricts;
+  }
+
   String? _getDateTimeText(DateTime? dateTime) {
-    if (dateTime == null) return dateFormat.format(DateTime.now());
+    if (dateTime == null) {
+      return AppLocalizations.of(context)?.dateValidationError;
+    }
     return dateFormat.format(dateTime);
   }
 
-  void onSaveSelectDistrict(List<District> newSelectedDistricts) {
-    selectedDistricts = newSelectedDistricts;
+  bool _validateForm(BuildContext context) {
+    final isTextFormFieldValid = _formKey.currentState?.validate();
+    final atLeastOneDistrict = selectedDistricts.isNotEmpty;
+
+    // Start date and end date are selected
+    if (startDateTime == null || endDateTime == null) {
+      _showSnackBar(AppLocalizations.of(context)?.dateValidationError, context);
+      return false;
+    }
+
+    // At least one district is selected
+    if (!atLeastOneDistrict) {
+      _showSnackBar(AppLocalizations.of(context)?.atLeastOneDistrict, context);
+      return false;
+    }
+
+    // Location not validated
+    if (locationController.text.isEmpty ||
+        locationController.text.length > 30) {
+      _showSnackBar(
+        AppLocalizations.of(context)?.locationValidationError,
+        context,
+      );
+      return false;
+    }
+
+    return (isTextFormFieldValid == true && atLeastOneDistrict);
+  }
+
+  _showSnackBar(String? message, BuildContext context) {
+    CustomSnackBar(
+      message: message,
+      type: SnackBarType.error,
+    ).display(context);
   }
 
   @override
@@ -65,20 +102,27 @@ class CreateTennisMatchScreenState
         child: ListView(
           children: [
             const SizedBox(height: 16),
-            CustomTextFormField(
-              controller: startDateController,
-              textInputType: TextInputType.datetime,
-              labelText: AppLocalizations.of(context)?.startDateTime,
-              onTap: () async {
-                final DateTime? selectedDate =
-                    await sheetDateTimePicker(context: context);
+            Container(
+              alignment: Alignment.centerLeft,
+              padding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
+              child: GestureDetector(
+                onTap: () async {
+                  final DateTime? selectedDate =
+                      await sheetDateTimePicker(context: context);
+                  if (selectedDate == null) return;
 
-                if (selectedDate == null) return;
-
-                setState(() {
-                  startDateTime = selectedDate;
-                });
-              },
+                  setState(() {
+                    startDateTime = selectedDate;
+                  });
+                },
+                child: Row(
+                  children: [
+                    CustomText(
+                        AppLocalizations.of(context)?.startDateTime ?? ""),
+                    CustomText(_getDateTimeText(startDateTime))
+                  ],
+                ),
+              ),
             ),
             const SizedBox(height: 16),
             Container(
@@ -105,7 +149,7 @@ class CreateTennisMatchScreenState
             Container(
               alignment: Alignment.centerLeft,
               child: DropdownButton<MatchType>(
-                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  padding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
                   value: dropdownValue,
                   icon: const Icon(Icons.arrow_downward),
                   elevation: 16,
@@ -141,6 +185,25 @@ class CreateTennisMatchScreenState
               labelText: AppLocalizations.of(context)?.remarks,
               maxLines: 3,
             ),
+            Container(
+              margin: const EdgeInsets.symmetric(horizontal: 40),
+              child: ElevatedButton(
+                onPressed: () {
+                  if (_validateForm(context)) {
+                    print("submit");
+                  } else {
+                    print("not valid");
+                  }
+                },
+                style: const ButtonStyle(
+                  padding: MaterialStatePropertyAll(
+                    EdgeInsets.symmetric(vertical: 20),
+                  ),
+                ),
+                child: CustomText(AppLocalizations.of(context)?.submit ?? ""),
+              ),
+            ),
+            const SizedBox(height: 40),
           ],
         ),
       ),
