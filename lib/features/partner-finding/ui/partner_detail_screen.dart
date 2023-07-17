72412c8930e2e621f3a572afc30f8a5abbf33cc3
charleskan <71898304+charleskan@users.noreply.github.com>
2023-07-17 20:25:11 +0800

    feat: changed partner_detail ui

--- lib/features/partner-finding/ui/partner_detail_screen.dart
+++ lib/features/partner-finding/ui/partner_detail_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:play_tennis_hk/components/custom_card.dart';
 import 'package:play_tennis_hk/components/custom_text.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:play_tennis_hk/domain/district.dart';
@@ -27,6 +28,82 @@ class PartnerDetailScreen extends StatelessWidget {
       ),
       body: ListView(
         children: [
+          Container(
+            padding: const EdgeInsets.all(12),
+            child: CustomCard(
+              child: ListView(
+                shrinkWrap: true,
+                children: [
+                  Row(
+                    children: [
+                      Container(
+                        padding: const EdgeInsets.all(12),
+                        child: const Icon(
+                          Icons.account_circle,
+                          size: 66,
+                        ),
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          CustomText(
+                            userProfile.username,
+                            fontWeight: FontWeight.bold,
+                          ),
+                          Row(
+                            children: [
+                              CustomText(
+                                  AppLocalizations.of(context)?.ntrpLevel),
+                              const SizedBox(width: 4),
+                              CustomText(userProfile.ntrpLevel.toString()),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Container(
+                        padding: const EdgeInsets.all(12),
+                        child: const Icon(
+                          Icons.place,
+                        ),
+                      ),
+                      CustomText(
+                        userProfile.districts
+                                ?.map(
+                                  (district) => district.toLocalizedName(
+                                      AppLocalizations.of(context)?.localeName),
+                                )
+                                .toList()
+                                .join(", ") ??
+                            "",
+                      ),
+                    ],
+                  ),
+                  if (userProfile.telegram != null) ...[
+                    ListTile(
+                      title: CustomText(AppLocalizations.of(context)?.telegram),
+                      subtitle: CustomText(userProfile.telegram),
+                    ),
+                  ],
+                  if (userProfile.whatsapp != null) ...[
+                    ListTile(
+                      title: CustomText(AppLocalizations.of(context)?.whatsapp),
+                      subtitle: CustomText(userProfile.whatsapp),
+                    ),
+                  ],
+                  if (userProfile.signal != null) ...[
+                    ListTile(
+                      title: CustomText(AppLocalizations.of(context)?.signal),
+                      subtitle: CustomText(userProfile.signal),
+                    ),
+                  ],
+                ],
+              ),
+            ),
+          ),
           if (tennisMatch != null) ...[
             Container(
               padding: const EdgeInsets.all(12),
@@ -36,39 +113,6 @@ class PartnerDetailScreen extends StatelessWidget {
               ),
             ),
           ],
-          ListTile(
-            title: CustomText(AppLocalizations.of(context)?.username),
-            subtitle: CustomText(userProfile.username),
-          ),
-          ListTile(
-            title: CustomText(AppLocalizations.of(context)?.ntrpLevel),
-            subtitle: CustomText(userProfile.ntrpLevel.toString()),
-          ),
-          ListTile(
-            title: CustomText(AppLocalizations.of(context)?.district),
-            subtitle: CustomText(
-              userProfile.districts
-                      ?.map(
-                        (district) => district.toLocalizedName(
-                            AppLocalizations.of(context)?.localeName),
-                      )
-                      .toList()
-                      .join(", ") ??
-                  "",
-            ),
-          ),
-          ListTile(
-            title: CustomText(AppLocalizations.of(context)?.telegram),
-            subtitle: CustomText(userProfile.telegram),
-          ),
-          ListTile(
-            title: CustomText(AppLocalizations.of(context)?.whatsapp),
-            subtitle: CustomText(userProfile.whatsapp),
-          ),
-          ListTile(
-            title: CustomText(AppLocalizations.of(context)?.signal),
-            subtitle: CustomText(userProfile.signal),
-          ),
         ],
       ),
     );
