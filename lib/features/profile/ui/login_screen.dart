dfd74c9bfd5f7f45b3fa356c7b91d78d08b371f9
charleskan <71898304+charleskan@users.noreply.github.com>
2023-07-20 02:53:39 +0800

    test: login username and password using db data.

--- lib/features/profile/ui/login_screen.dart
+++ lib/features/profile/ui/login_screen.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:play_tennis_hk/components/custom_text.dart';
 import 'package:play_tennis_hk/components/custom_text_form_field.dart';
-import 'package:play_tennis_hk/features/profile/ui/profile_screen.dart';
 import 'package:play_tennis_hk/features/profile/domain/providers/user_profile_provider.dart';
+import 'package:play_tennis_hk/features/profile/ui/profile_screen.dart';
 
 class LoginScreen extends ConsumerStatefulWidget {
   const LoginScreen({super.key});
@@ -64,9 +64,10 @@ class LoginScreenState extends ConsumerState<LoginScreen> {
             ),
             ElevatedButton(
               onPressed: () {
+                
                 ref
                     .read(userProfileProvider.notifier)
-                    .login("username", "password");
+                    .login("Hemingway", "Test0000");
               },
               style: const ButtonStyle(
                 padding: MaterialStatePropertyAll(
